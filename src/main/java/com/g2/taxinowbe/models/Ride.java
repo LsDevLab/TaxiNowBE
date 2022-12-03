8801c528081131d338bd7a432ebba4814c37d0b6
Luigi Schiavone <luigisc98@gmail.com>
2022-12-03 16:33:04 +0100

    Ride model class created.

---
+++ src/main/java/com/g2/taxinowbe/models/Ride.java
@@ -0,0 +1,127 @@
+package com.g2.taxinowbe.models;
+
+import java.time.LocalDateTime;
+
+
+/**
+ * Class representing a taxi ride.
+ */
+public class Ride {
+
+    /**
+     * ID of the customer who creates the ride request
+     */
+    private final String customerID;
+    /**
+     * The state of the ride (pending, accepted, completed)
+     */
+    private RideState state;
+    /**
+     * The ID of the driver who accepts the ride
+     */
+    private String acceptedByDriverID;
+    /**
+     * The number of passengers of the ride
+     */
+    private final int numOfPassengers;
+    /**
+     * The date of creation of the ride
+     */
+    private final LocalDateTime createdOn;
+    /**
+     * The max price the customer can pay in euros for the ride
+     */
+    private final Float maxAllowedPrice;
+    /**
+     * The price esthablished by the driver when he accepts
+     */
+    private Float acceptationPrice;
+
+    /**
+     * Creates a new Ride object, setting it as pending
+     *
+     * @param customerID ID of the driver who accepts the ride
+     * @param numOfPassengers the number of passengers
+     * @param createdOn the date of creation
+     * @param maxAllowedPrice max price the customer can pay in euros for the ride
+     */
+    public Ride(String customerID, int numOfPassengers, LocalDateTime createdOn, Float maxAllowedPrice) {
+        this.customerID = customerID;
+        this.numOfPassengers = numOfPassengers;
+        this.createdOn = createdOn;
+        this.maxAllowedPrice = maxAllowedPrice;
+        this.state = RideState.PENDING;
+    }
+
+    /**
+     * Set the ride as accepted
+     *
+     * @param acceptedByDriverID the ID of the driver who accepted the ride
+     * @param acceptationPrice the acceptation price
+     */
+    public void setAccepted(String acceptedByDriverID, Float acceptationPrice) {
+        this.acceptedByDriverID = acceptedByDriverID;
+        this.acceptationPrice = acceptationPrice;
+        this.state = RideState.ACCEPTED;
+    }
+
+    /**
+     * Set the ride as not accepted (pending)
+     */
+    public void setPending() {
+        this.acceptedByDriverID = null;
+        this.acceptationPrice = null;
+        this.state = RideState.PENDING;
+    }
+
+    /**
+     * Set the ride as completed
+     */
+    public void setCompleted(){
+        this.state = RideState.COMPLETED;
+    }
+
+    public String getCustomerID() {
+        return customerID;
+    }
+
+    public String getAcceptedByDriverID() {
+        return acceptedByDriverID;
+    }
+
+    public Float getAcceptationPrice() {
+        return acceptationPrice;
+    }
+
+    public int getNumOfPassengers() {
+        return numOfPassengers;
+    }
+
+    public LocalDateTime getCreatedOn() {
+        return createdOn;
+    }
+
+    public Float getMaxAllowedPrice() {
+        return maxAllowedPrice;
+    }
+
+    /**
+     * @return if the ride is accepted
+     */
+    public boolean isAccepted(){
+        return acceptedByDriverID != null;
+    }
+
+    @Override
+    public String toString() {
+        return "Ride{" +
+                "customerID='" + customerID + '\'' +
+                ", state=" + state +
+                ", acceptedByDriverID='" + acceptedByDriverID + '\'' +
+                ", numOfPassengers=" + numOfPassengers +
+                ", createdOn=" + createdOn +
+                ", maxAllowedPrice=" + maxAllowedPrice +
+                ", acceptationPrice=" + acceptationPrice +
+                '}';
+    }
+}
---
+++ src/main/java/com/g2/taxinowbe/models/RideState.java
@@ -0,0 +1,25 @@
+package com.g2.taxinowbe.models;
+
+/**
+ * Possible states for a ride
+ */
+public enum RideState {
+    PENDING,
+    ACCEPTED,
+    COMPLETED;
+
+    @Override
+    public String toString() {
+        switch (this) {
+            case PENDING:
+                return "Pending";
+            case ACCEPTED:
+                return "Accepted";
+            case COMPLETED:
+                return "Completed";
+            default:
+                return "No state";
+        }
+    }
+
+}
