5c900809bd57955ea9fc38aed8b23746012e8424
Luigi Schiavone <luigisc98@gmail.com>
2022-12-12 16:23:04 +0100

    added multicast test

---
+++ src/main/java/com/g2/taxinowbe/test/UDPClientM.java
@@ -0,0 +1,37 @@
+package com.g2.taxinowbe.test;
+
+import java.io.IOException;
+import java.net.*;
+import java.util.Arrays;
+import java.util.Enumeration;
+
+public class UDPClientM {
+
+    private static int SERVER_PORT = 7777;
+    private static int CLIENT_PORT = 7778;
+
+    public static void main(String[] args) throws IOException {
+
+       Enumeration<NetworkInterface> ns= NetworkInterface.getNetworkInterfaces();
+       while (ns.hasMoreElements())
+           System.out.println(ns.nextElement().getName());
+
+        InetAddress mcastaddr = InetAddress.getByName("234.5.6.7");
+        InetSocketAddress group = new InetSocketAddress(mcastaddr, 6789);
+        NetworkInterface netIf = NetworkInterface.getByName("0.0.0.0");
+        MulticastSocket s = new MulticastSocket(7778);
+
+        s.joinGroup(group, netIf);
+
+        while (true) {
+            byte[] buf = new byte[1000];
+            DatagramPacket recv = new DatagramPacket(buf, buf.length);
+            s.receive(recv);
+            // OK, I'm done talking - leave the group...
+            s.leaveGroup(group, netIf);
+
+            System.out.println("Ricevuti: " + recv.getLength() + " bytes");
+        }
+    }
+
+}
---
+++ src/main/java/com/g2/taxinowbe/test/UDPServerM.java
@@ -0,0 +1,32 @@
+package com.g2.taxinowbe.test;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+
+
+public class UDPServerM {
+
+    private static int TRANSISSION_PORT = 7778;
+
+    private static String[] file = {"Line1", "Line 2", "Line3"};
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        MulticastSocket sock = new MulticastSocket();
+        InetAddress addr = InetAddress.getByName("230.0.0.0");
+
+        System.out.println(sock.getNetworkInterface().getName());
+        while (true) {
+            System.out.println("SENDING");
+            for (String line : file) {
+                byte[] lineBytes = line.getBytes();
+                DatagramPacket packet = new DatagramPacket(lineBytes, lineBytes.length, addr, TRANSISSION_PORT);
+                sock.send(packet);
+                Thread.sleep(500);
+            }
+        }
+    }
+
+}
